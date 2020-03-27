ca62a2c013d9f87a08c5cef6e2cb91a069b390dd
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-03-27 22:00:28 +0300

    Designed the UI.

---
+++ app/src/main/java/codes/malukimuthusi/newsdemoapp/DetailView/DetailViewModel.kt
@@ -0,0 +1,7 @@
+package codes.malukimuthusi.newsdemoapp.DetailView
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/codes/malukimuthusi/newsdemoapp/ListView/ListViewModel.kt
@@ -0,0 +1,7 @@
+package codes.malukimuthusi.newsdemoapp.ListView
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
