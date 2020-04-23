522c4072fabc55d3e4bd5e44c5ae6a2b3fc02d2c
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-04-23 17:08:40 +0300

    Debugged Gradle, build issues

---
+++ app/src/main/java/codes/malukimuthusi/newsdemoapp/articleDetails/ui/main/DetailsFragment.kt
@@ -0,0 +1,27 @@
+package codes.malukimuthusi.newsdemoapp.articleDetails.ui.main
+
+//class DetailsFragment : Fragment() {
+//
+//    companion object {
+//        fun newInstance() = DetailsFragment()
+//    }
+//
+//    private lateinit var viewModel: DetailsViewModel
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//
+////        val viewModel by viewModels<DetailsViewModel>
+//        return inflater.inflate(R.layout.main_fragment, container, false)
+//    }
+//
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+////        val viewModel by viewModels<DetailsViewModel>
+////        viewModel = ViewModelProviders.of(this).get(DetailsViewModel::class.java)
+//        // TODO: Use the ViewModel
+//    }
+//
+//}
---
+++ app/src/main/java/codes/malukimuthusi/newsdemoapp/articleDetails/ui/main/DetailsViewModel.kt
@@ -0,0 +1,7 @@
+package codes.malukimuthusi.newsdemoapp.articleDetails.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class DetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
